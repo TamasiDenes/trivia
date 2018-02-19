73f4d9c00b8141e287c395eca3f4353b645ae81f
TamasiDenes <tamasidenes@gmail.com>
2018-02-19 14:02:00 +0100

    Update Game.h

--- C++/Game.h
+++ C++/Game.h
@@ -21,6 +21,10 @@ public:
 	void addPlayer(string playerName);
 
 	bool roll();
+	bool answerIt( unsigned hitRatio = 10)
+	{
+		return (rand() % 100 < hitRation );
+	}
 	void NextPlayer() { players.NextPlayer(); };
 	bool correctAnswer();
 	void wrongAnswer();
